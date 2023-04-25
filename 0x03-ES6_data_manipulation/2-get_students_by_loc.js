6db25bb9bd019c2904e118013e9558c2c6a56a2d
Abdulazeez <azeezgaj@gmail.com>
2023-04-25 07:26:53 -0700

    read

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++
@@ -1,8 +0,0 @@
-export default function getStudentsByLocation(A, B) {
-    const result = A.filter((a) => {
-        if (a.location === B) {
-            return a;
-        }
-    })
-    return result;
-}
--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++
@@ -1,7 +0,0 @@
-export default function getStudentIdsSum(A) {
-    const arr = A.map((d) => {return d.id;});
-    const result = arr.reduce((a, b) => {
-        return a + b;
-    })
-    return result;
-}
